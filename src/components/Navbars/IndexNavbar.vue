c639fa727a94637c34992abd054f82d36146c68a
firmansyah <emank.tunk@gmail.com>
2021-06-19 18:37:41 +0700

    Padi Group Malang

--- src/components/Navbars/IndexNavbar.vue
+++ src/components/Navbars/IndexNavbar.vue
@@ -1,23 +1,53 @@
 <template>
   <nav
-    class="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow"
+    class="
+      top-0
+      fixed
+      z-50
+      w-full
+      flex flex-wrap
+      items-center
+      justify-between
+      px-2
+      py-3
+      navbar-expand-lg
+      bg-white
+      shadow
+    "
   >
     <div
       class="container px-4 mx-auto flex flex-wrap items-center justify-between"
     >
       <div
-        class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
+        class="
+          w-full
+          relative
+          flex
+          justify-between
+          lg:w-auto
+          lg:static
+          lg:block
+          lg:justify-start
+        "
       >
         <router-link to="/">
-          <a
-            class="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-            href="#pablo"
-          >
-            Vue Notus
-          </a>
+          <img style="height: 30px; width: auto" :src="logo" />
         </router-link>
         <button
-          class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+          class="
+            cursor-pointer
+            text-xl
+            leading-none
+            px-3
+            py-1
+            border border-solid border-transparent
+            rounded
+            bg-transparent
+            block
+            lg:hidden
+            outline-none
+            focus:outline-none
+          "
           type="button"
           v-on:click="setNavbarOpen"
         >
@@ -29,7 +59,7 @@
         :class="[navbarOpen ? 'block' : 'hidden']"
         id="example-navbar-warning"
       >
-        <ul class="flex flex-col lg:flex-row list-none mr-auto">
+        <!-- <ul class="flex flex-col lg:flex-row list-none mr-auto">
           <li class="flex items-center">
             <a
               class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
@@ -41,52 +71,91 @@
               Docs
             </a>
           </li>
-        </ul>
+        </ul> -->
         <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
           <li class="flex items-center">
             <index-dropdown />
           </li>
           <li class="flex items-center">
-            <a
-              class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
-              href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdemos.creative-tim.com%2Fvue-notus%2F%23%2F"
-              target="_blank"
+            <span
+              class="
+                hover:text-blueGray-500
+                text-blueGray-700
+                px-3
+                py-2
+                flex
+                items-center
+                text-xs
+                uppercase
+                font-bold
+              "
+              >Home</span
             >
-              <i class="text-blueGray-400 fab fa-facebook text-lg leading-lg" />
-              <span class="lg:hidden inline-block ml-2">Share</span>
-            </a>
           </li>
 
           <li class="flex items-center">
-            <a
-              class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
-              href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fdemos.creative-tim.com%2Fvue-notus%2F%23%2F&text=Start%20your%20development%20with%20a%20Free%20Tailwind%20CSS%20and%20VueJS%20UI%20Kit%20and%20Admin.%20Let%20Vue%20Notus%20amaze%20you%20with%20its%20cool%20features%20and%20build%20tools%20and%20get%20your%20project%20to%20a%20whole%20new%20level.%20"
-              target="_blank"
+            <span
+              class="
+                hover:text-blueGray-500
+                text-blueGray-700
+                px-3
+                py-2
+                flex
+                items-center
+                text-xs
+                uppercase
+                font-bold
+              "
+              >About</span
             >
-              <i class="text-blueGray-400 fab fa-twitter text-lg leading-lg" />
-              <span class="lg:hidden inline-block ml-2">Tweet</span>
-            </a>
           </li>
 
           <li class="flex items-center">
-            <a
-              class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
-              href="https://github.com/creativetimofficial/vue-notus?ref=vn-index-navbar"
-              target="_blank"
+            <span
+              class="
+                hover:text-blueGray-500
+                text-blueGray-700
+                px-3
+                py-2
+                flex
+                items-center
+                text-xs
+                uppercase
+                font-bold
+              "
+              >Menu</span
             >
-              <i class="text-blueGray-400 fab fa-github text-lg leading-lg" />
-              <span class="lg:hidden inline-block ml-2">Star</span>
-            </a>
           </li>
 
-          <li class="flex items-center">
+          <!-- <li class="flex items-center">
             <button
-              class="bg-emerald-500 text-white active:bg-emerald-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+              class="
+                bg-emerald-500
+                text-white
+                active:bg-emerald-600
+                text-xs
+                font-bold
+                uppercase
+                px-4
+                py-2
+                rounded
+                shadow
+                hover:shadow-lg
+                outline-none
+                focus:outline-none
+                lg:mr-1
+                lg:mb-0
+                ml-3
+                mb-3
+                ease-linear
+                transition-all
+                duration-150
+              "
               type="button"
             >
               <i class="fas fa-arrow-alt-circle-down"></i> Download
             </button>
-          </li>
+          </li> -->
         </ul>
       </div>
     </div>
@@ -95,10 +164,11 @@
 
 <script>
 import IndexDropdown from "@/components/Dropdowns/IndexDropdown.vue";
-
+import logo from "../../assets/img/logo.png";
 export default {
   data() {
     return {
+      logo,
       navbarOpen: false,
     };
   },
