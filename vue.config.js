0799be5e2e519fdb1514efc076b049b101fb11d8
firmansyah <emank.tunk@gmail.com>
2021-06-19 18:59:49 +0700

    Padi Group Malang

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     port: process.env.PORT || 3000,
     host: '0.0.0.0'
   },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/production-sub-path/'
-    : '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/production-sub-path/'
+  //   : '/'
 };
